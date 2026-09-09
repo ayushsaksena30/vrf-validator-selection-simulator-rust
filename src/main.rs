a55ba3c3c3c48623ceb35478444aed7e846308d1
Ayush Saksena <asaksena100@gmail.com>
2026-09-09 17:36:15 +0530

    Day 3: implement VRF output computation via HMAC-SHA256

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ use ed25519_dalek::SigningKey;
 use rand::rngs::OsRng;
 use node::{Node};
 use hex::encode;
+use hmac::{Hmac, KeyInit, Mac};
+use sha2::Sha256;
+
+type HmacSha256 = Hmac<Sha256>;
 
 fn main() {
 
     let mut rng= OsRng{};
+
     for n in 1..=10{
         let node = Node{
             id: n as u32,
@@ -15,6 +20,15 @@ fn main() {
             is_malicious: false,
         };
 
-        println!("Node id- {}, Public Key- {}",node.id, encode(node.signing_key.verifying_key().to_bytes()));
+        println!("Node id- {}, Public Key- {}, VRF- {}",node.id, encode(node.signing_key.verifying_key().to_bytes()), compute_vrf(&node, 1));
     }
+}
+
+fn compute_vrf(node: &Node, round: u32) -> u64{
+    let key = node.signing_key.to_bytes();
+    let mut mac=HmacSha256::new_from_slice(&key).unwrap();
+    mac.update(&round.to_be_bytes());
+    let res=mac.finalize().into_bytes();
+
+    u64::from_be_bytes(res[0..8].try_into().unwrap())
 }
